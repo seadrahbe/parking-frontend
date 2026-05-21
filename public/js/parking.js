5584b6b84303b6abbaf779bba2cd47b20e644567
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-05-21 14:27:05 -0700

    Lab completed

--- public/js/parking.js
+++ public/js/parking.js
@@ -1,5 +1,82 @@
 window.onload = () => {
-    
+    const button = document.querySelector("#load-button");
+    button.onclick = loadRecordsFetch;
+
+    const form = document.querySelector("#parking-form");
+    form.onsubmit = handleFormAxios;
+}
+
+async function handleFormAxios() {
+    event.preventDefault();
+
+    try {
+        const inputs = readFormEntries();
+        const url = "http://localhost:8001/api/fake/campus/parkingSpots";
+        const response = await axios.post(url, inputs);
+    } catch(err) {
+        showError(err.message, err.status);
+    }
+}
+
+async function handleFormFetch() {
+    event.preventDefault(); // stops page refresh
+
+    try {
+        const inputs = readFormEntries();
+        const url = "http://localhost:8001/api/fake/campus/parkingSpots";
+        const config = {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(inputs)
+        }
+
+        const response = await fetch(url, config);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+async function loadRecordsAxios() {
+    try {
+        const url = "http://localhost:8001/api/fake/campus/parkingSpots";
+        const response = await axios.get(url, { timeout: 5000 });
+
+        hideError();
+        showRecords(response.data);
+    } catch(err) {
+        showError(err.status, err.message);
+    }
+
+}
+
+async function loadRecordsFetch() {
+    try {
+        const url = "http://localhost:8001/api/fake/campus/parkingSpots?status=500";
+        const config = { method: "get" };
+        const response = await fetch(uri, config);
+        if (!response.ok) return showError(response.statusText, response.status);
+        else hideError();
+
+        const records = await response.json();
+        showRecords(records);
+
+    } catch (err) {
+        console.log(err); 
+        console.log("Network failure!");
+    }
+}
+
+function showRecords(records) {
+    const recordsArea = document.querySelector("#records");
+    recordsArea.innerHTML = ''; // remove all content in panel
+
+    // add records to panel
+    for (const record of records.data) {
+        const html = getParkingSpotCard(record);
+        recordsArea.innerHTML = html;
+    }
 }
 
 function readFormEntries() {
